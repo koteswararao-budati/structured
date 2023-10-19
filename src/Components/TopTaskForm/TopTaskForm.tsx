42c18748c0821225981d21c4cc36fbe31cec88ab
koti <koteswararaob94@outlook.com>
2023-10-18 22:27:51 -0500

    Updated UI

--- src/Components/TopTaskForm/TopTaskForm.tsx
+++ src/Components/TopTaskForm/TopTaskForm.tsx
@@ -1,46 +1,37 @@
 import styles from "./TopTaskForm.module.css"
+import close from "../../assets/close.svg"
+import {useContext} from "react";
+import {AppRenderState} from "../../Context/AppRenderContext.tsx";
+import {MONTHS} from "../../Constants/CONSTANTS.ts";
 
 function TopTaskForm() {
+    const {selectedDate} = useContext(AppRenderState).state
     return (
-        <div className={styles.topTaskForm}>
+        <div className={styles.container}>
+            <div className={styles.close}>
+                <h5>{selectedDate.getDate()}-{MONTHS[selectedDate.getMonth()]}-{selectedDate.getFullYear()}</h5>
+                <button>
+                    <img src={close} alt={"close"}/>
+                </button>
+            </div>
             <div className={styles.tasks}>
-                <h3>Top 3 Task of The Day</h3>
+                <h3>Add Task</h3>
                 <div>
-                    <label>First Task</label>
-                    <input type={"text"} placeholder={"Hello"}
+                    <input className={styles.inputTask} type={"text"} placeholder={"Add Task"}
                            onChange={((e) => console.log(e.target.value))}/>
+                    <div className={styles.flags}>
+                        <label>Flag: </label>
+                        <input className={styles.red} type={"radio"} name={"flag"} value={"red"}/>
+                        <input className={styles.yellow} type={"radio"} name={"flag"} value={"yellow"}/>
+                        <input className={styles.green} type={"radio"} name={"flag"} value={"green"}/>
+                    </div>
                 </div>
-                <div>
-                    <label>First Task</label>
-                    <input type={"text"} placeholder={"Hello"}
-                           onChange={((e) => console.log(e.target.value))}/>
+                <div className={styles.submit}>
+                    <button className={styles.submitTask}>Add One More</button>
+                    <button className={styles.submitTask}>Done</button>
                 </div>
-                <div>
-                    <label>First Task</label>
-                    <input type={"text"} placeholder={"Hello"}
-                           onChange={((e) => console.log(e.target.value))}/>
-                </div>
+            </div>
 
-            </div>
-            <div className={styles.tasks}>
-                <h3>Top 3 Good to Complete tasks</h3>
-                <div>
-                    <label>Task</label>
-                    <input
-                        onChange={((e) => console.log(e.target.value))}/>
-                </div>
-                <div>
-                    <label>First Task</label>
-                    <input type={"text"} placeholder={"Hello"}
-                           onChange={((e) => console.log(e.target.value))}/>
-                </div>
-                <div>
-                    <label>First Task</label>
-                    <input type={"text"} placeholder={"Hello"}
-                           onChange={((e) => console.log(e.target.value))}/>
-                </div>
-            </div>
-            <button className={styles.submitTask}>Submit</button>
         </div>
     )
 }
